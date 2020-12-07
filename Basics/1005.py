900191e6e4e3be05e6b781e2803f53d752f49591
isoBello <isabellamfreitas2@gmail.com>
2020-12-07 19:52:53 -0300

    Using try/except

--- Basics/1005.py
+++ Basics/1005.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
-def media(A, B):
-    pesos = 3.5 +  7.5
-    media = (A + B)/pesos
-    print('MEDIA = {0:1.5f}'.format(media))
-
+def media(notas):
+    try:
+        media = (notas[0] + notas[1])/(notas[2] + notas[3])
+        print('MEDIA = {0:1.5f}'.format(media))
+    except expression as identifier:
+        pass
 
 if __name__ == "__main__":
     A = float(input())
     B = float(input())
-    media(A*3.5, B*7.5)
+    notas = [A*3.5, B*7.5, 3.5, 7.5]
+    media(notas)
