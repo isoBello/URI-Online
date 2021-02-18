bd295d71dde02ef1440dcd7549f09ef8cd00d27e
isoBello <isabellamfreitas2@gmail.com>
2021-02-18 12:33:40 -0300

    Solved with dinamic programming

--- Paradigmas/2842.py
+++ Paradigmas/2842.py
@@ -2,24 +2,32 @@
 
 # Solving Dabriel e Suas Strings
 def concatenar(a, b):
-    if len(a) > len(b):
-        if b in a:
-            new = a
-        else:
-            new = b + a
-            new = "".join(list(dict.fromkeys(new)))
+    size_a = len(a)
+    size_b = len(b)
+    if size_a > size_b and b in a:
+        return size_a
+    elif size_b > size_a and a in b:
+        return size_b
     else:
-        if a in b:
-            new = b
-        else:
-            new = a + b
-            new = "".join(list(dict.fromkeys(new)))
+        k = size_a + 2
+        n = size_b + 2
+        dinamic = [[0] * n for _ in range(k)]
 
-    print(new, len(new))
+        for i in range(k - 1):
+            for j in range(n - 1):
+                if not i:
+                    dinamic[i][j] = j
+                elif not j:
+                    dinamic[i][j] = i
+                elif a[i - 1] == b[j - 1]:
+                    dinamic[i][j] = 1 + dinamic[i - 1][j - 1]
+                else:
+                    dinamic[i][j] = 1 + min(dinamic[i - 1][j], dinamic[i][j - 1])
+        return dinamic[size_a][size_b]
 
 
 if __name__ == "__main__":
     A = input()
     B = input()
 
-    concatenar(A, B)
+    print(concatenar(A, B))
