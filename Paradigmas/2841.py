f91d2c4dae9f6bb30b6b104af4bdee4baad14892
isoBello <isabellamfreitas2@gmail.com>
2021-02-20 14:02:16 -0300

    Solved with PD in trees

--- Paradigmas/2841.py
+++ Paradigmas/2841.py
@@ -2,51 +2,56 @@
 
 
 # Solving Dabriel e Sua Festa
-def dfs(node, parent, d1, d2, profit, t):
+def dfs(node, parent, dp, animation, t):
     sum1 = 0
     sum2 = 0
+
     for child in t[node]:
         if child == parent:
             continue
-        dfs(child, node, d1, d2, profit, t)
+
+        dfs(child, node, dp, animation, t)
 
         # Inclui o n-th nó e não inclui seus filhos
-        sum1 += d2[child]
+        sum1 += dp[child][1]
 
         # Não inclui o nó, então inclui seus filhos ou não
-        sum2 == max(d1[child], d2[child])
-    d1[node] = profit[node] + sum1
-    d1[node] = sum2
+        sum2 += max(dp[child][0], dp[child][1])
+    dp[node][0] = profit[node] + sum1
+    dp[node][1] = sum2
 
 
 if __name__ == "__main__":
     pessoas = int(input())
     fun = list(map(int, input().split(" ")))
     chefes = list(map(int, input().split(" ")))
-    tree = {}
+
+    k = pessoas + 1
+    tree = [[] for i in range(k)]
 
     # Dummys
-    chefes.insert(0, -1)
-    chefes.insert(0, -1)
+    chefes.insert(0, 0)
+    chefes.insert(1, 0)
+    fun.insert(0, 0)
 
-    fun.insert(0, -1)
-    fun.insert(pessoas+1, -1)
-
-    for i in range(1, pessoas + 1):
-        if i in chefes:
-            subordinados = [j for j, x in enumerate(chefes) if x == i]
-            try:
-                subordinados.append(chefes[i])
-            except ValueError:
-                continue
-        else:
+    for i in range(1, k):
+        try:
             subordinados = chefes[i]
             subordinados = [subordinados]
+        except ValueError:
+            continue
+
+        if i in chefes:
+            p = [j for j, x in enumerate(chefes) if x == i]
+            subordinados = subordinados + p
         tree[i] = subordinados
 
-    dp1 = [0 for i in range(pessoas + 2)]
-    dp2 = [0 for i in range(pessoas + 2)]
+    profit = [0 for i in range(k)]
+    for i in range(1, k):
+        profit[i] = fun[i]
 
-    tree[1].remove(-1)
-    dfs(1, 1, dp1, dp2, fun, tree)
-    print(max(dp1[1], dp2[1]))
+    dp = [[0] * 3 for _ in range(k)]
+    tree[1].remove(0)
+
+    dfs(1, 1, dp, profit, tree)
+    print(max(dp[1][0], dp[1][1]))
