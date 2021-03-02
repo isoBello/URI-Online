1a306fc42b4a440ad2acb1b7be0f66f3d017782d
isoBello <isabellamfreitas2@gmail.com>
2021-03-02 19:10:27 -0300

    Trying with class Bag(), based on Knapsack Problem

--- Paradigmas/1767.py
+++ Paradigmas/1767.py
@@ -9,6 +9,35 @@ class Bag:
         self.peso = w
 
 
+def solve(P, packages, dp):
+    global PACKS, W
+    for i in range(P):
+        for j in range(W):
+            if not i or not j:
+                dp[i][j] = 0
+            else:
+                if packages[i].peso > j:
+                    # O brinquedo não pode ser colocado no saco
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    # Analisamos se é melhor colocar ou não colocar o brinquedo no saco, baseado no ganho (max)
+                    dp[i][j] = max(packages[i].toy + dp[i - 1][j - packages[i].peso], dp[i - 1][j])
+    # A resposta ficará em dp[PACKS][W]
+    # Agora precisamos analisar a quantidade de pacotes utilizados
+    return dp[PACKS][W], count_packs(P, dp, packages)
+
+
+def count_packs(P, dp, packs):
+    qtPacks = 0
+    qtPeso = 0
+    for i in range(P, 0, -1):
+        for j in range(W, 0, -1):
+            if dp[i][j] != dp[i - 1][j]:
+                qtPacks += 1
+                qtPeso += packs[i].peso
+                if j - packs[i].peso >= 0:
+                    j -= packs[i].peso
+    return qtPeso, qtPacks
 
 
 if __name__ == "__main__":
@@ -24,5 +53,7 @@ if __name__ == "__main__":
         for i in range(1, pac + 1):
             qt, peso = map(int, input().split(" "))
             saco[i].append(Bag(qt, peso))
-        # solve(saco, M)
+        answer, peso, pcts = solve(pac, saco, M)
+        output = "{} brinquedos" + "\n" + "Peso: {} kg" + "\n" + "sobra(m) {} pacote(s)" + "\n"
+        print(output.format(answer, peso, pcts))
         casos -= 1
