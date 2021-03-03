425c80f0a2e46c051b2afdfa1c26a4bf97e5ed98
isoBello <isabellamfreitas2@gmail.com>
2021-03-03 19:41:19 -0300

    Accepted

--- Paradigmas/1767.py
+++ Paradigmas/1767.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 
 
-def solve(N, M, packs):
+def solve(N, M, packages):
     for i in range(1, N + 1):
         for j in range(51):
-            if packs[i - 1][1] > j:    # O objeto pesa mais que a capacidade da mochila
+            if packages[i - 1][1] > j:    # O objeto pesa mais que a capacidade da mochila
                 M[i][j] = M[i - 1][j]
             else:
-                object = M[i - 1][j - packs[i - 1][1]]
-                anteriorObj = M[i - 1][j]
-                gain = packs[i - 1][0] + object[1]
-                if gain > anteriorObj[1]:
-                    M[i][j][0] = anteriorObj[0] + packs[i - 1][1]   # Peso
-                    M[i][j][1] = anteriorObj[1] + packs[i - 1][0]   # Valor
-                    M[i][j][2] = anteriorObj[2] - 1                 # Pacotes usados
+                item = M[i - 1][j - packages[i - 1][1]]
+                anteriorItem = M[i - 1][j]
+
+                if packages[i - 1][0] + item[1] > anteriorItem[1]:
+                    M[i][j][0] = item[0] + packages[i - 1][1]    # Peso
+                    M[i][j][1] = item[1] + packages[i - 1][0]   # Valor
+                    M[i][j][2] = item[2] - 1                 # Pacotes usados
                 else:    # Não houve melhora em colocar o objeto
-                    M[i][j] = anteriorObj
+                    M[i][j] = anteriorItem
     return M[N][50]
 
 
@@ -23,8 +23,6 @@ if __name__ == "__main__":
     # Solve Santa's Bag
     # Solução baseada no problema da mochila
     casos = int(input())
-    valor = {}
-    peso = {}
 
     while casos > 0:
         pac = int(input())
@@ -32,6 +30,8 @@ if __name__ == "__main__":
         M = [[[0, 0, pac] for _ in range(51)] for _ in range(pac + 1)]
 
         answer = solve(pac, M, packages)
-        print("{} brinquedos" + "\n" + "Peso: {} kg" + "\n" + "sobra(m) {} pacote(s)".format(answer[0], answer[1],
-                                                                                             answer[2]))
+        print("{} brinquedos".format(answer[1]))
+        print("Peso: {} kg".format(answer[0]))
+        print("sobra(m) {} pacote(s)".format(answer[2]))
+        print('')
         casos -= 1
