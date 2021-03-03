c82219e3144cd9492c49f3b5b9e3db70eb27bfdb
isoBello <isabellamfreitas2@gmail.com>
2021-03-03 19:47:32 -0300

    Reading input

--- Paradigmas/2665.py
+++ Paradigmas/2665.py
@@ -11,12 +11,25 @@ class Point:
         return self.y
 
 
+def solve(Ppoints):
+
+
+
+
+
+
 if __name__ == "__main__":
     P, Xa, Xb = map(int, input().split(" "))
     points = []
+
+    # Ancoras
+    A = Point(Xa, 0)
+    B = Point(Xb, 0)
 
     for point in range(P):
         x, y = map(int, input().split(" "))
         points.append(Point(x, y))
 
-
+    points.insert(0, A)
+    points.insert(len(points), B)
+    solve(points)
