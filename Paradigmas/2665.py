ab8d476c5efb35f3a62b88da14c23a62a93bbdd1
isoBello <isabellamfreitas2@gmail.com>
2021-03-04 19:29:31 -0300

    Wrong Answer, trying again

--- Paradigmas/2665.py
+++ Paradigmas/2665.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict
-import operator
 
 tree = defaultdict(list)
 INT_MAX = 10000
@@ -13,52 +12,19 @@ class Point:
 
 
 def pointIsInPolygon(pol, p):
-    extreme = Point(INT_MAX, p.y)
-    count = index = 0
-    while True:
-        next = (index + 1) % len(pol)
-        if theyIntersect(pol[index], pol[next], p, extreme):
-            if theOrientation(pol[index], p, pol[next]) == 0:
-                return onSegment(pol[index], p, pol[next])
-        count += 1
-        index = next
-        if index == 0:
-            break
-    k = (count % 2 == 1)
-    return k
+    k = len(pol)
+    c = False
+    for i in range(k):
+        j = (i + 1) % k
+        if compare(pol, p, i, j):
+            c = not c
+    return c
 
 
-def theyIntersect(a, b, c, d):
-    o1 = theOrientation(a, b, c)
-    o2 = theOrientation(a, b, d)
-    o3 = theOrientation(c, d, a)
-    o4 = theOrientation(c, d, b)
-
-    if o1 != o2 and o3 != o4 or (o1 == 0 and onSegment(a, c, b)) or (o2 == 0) and (onSegment(a, d, b)) or (o3 == 0) and (onSegment(c, a, d)) or (o4 == 0 and onSegment(c, b, d)):
-        return True
-    return False
-
-
-def theOrientation(p, q, r):
-    val = (((q.y - p.y) *
-            (r.x - q.x)) -
-           ((q.x - p.x) *
-            (r.y - q.y)))
-    if val == 0:
-        return 0
-    elif val > 0:
-        return 1
-    else:
-        return 2
-
-
-def onSegment(p, q, r):
-    if ((q.x <= max(p.x, r.x)) &
-            (q.x >= min(p.x, r.x)) &
-            (q.y <= max(p.y, r.y)) &
-            (q.y >= min(p.y, r.y))):
-        return True
-
+def compare(pol, p, i, j):
+    if pol[i].y <= p.y < pol[j].y or pol[j].y <= p.y < pol[i].y:
+        if p.x < pol[i].x + (pol[j].x - pol[i].x) * (p.y - p[i].y) / (pol[j].y - pol[i].y):
+            return True
     return False
 
 
