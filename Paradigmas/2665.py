4c0dfc34eef76809031662c06670d6f906aa2b48
isoBello <isabellamfreitas2@gmail.com>
2021-03-04 13:43:27 -0300

    Changing method

--- Paradigmas/2665.py
+++ Paradigmas/2665.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from collections import defaultdict
+tree = defaultdict(list)
 
 
 class Point:
@@ -7,42 +9,14 @@ class Point:
         self.y = yCoord
 
 
-# def semelhantesLLL(a, b, p1, p2):
-def getABC(a, b, p):
-    u = [a.x - b.x, a.y - b.y]  # AB
-    v = [a.x - p.x, a.y - p.y]  # AC
-    w = [b.x - p.x, b.y - p.y]  # BC
-
-    return [u, v, w]
-
-
-def semelhantes(t1, t2):
-    a = [t1[0][0]/t2[0][0], 0]
-    b = [t1[1][0]/t2[1][0], t1[1][1]/t2[1][1]]
-    c = [t1[2][0]/t2[2][0], t1[2][1]/t2[2][1]]
-    if a[0] == b[0] and b[0] == c[0]:
-        if a[1] == b[1] and b[1] == c[1]:
-            return True
-    return False
-
-
 def solve(a, b, pts):
-    answer = 1
-    tree = []
-    for i in range(len(pts)):
-        for j in range(len(pts)):
-            if i == j:
+    for i in range(len(pts) + 1):
+        polygon = [a, pts[i], b]
+        for j in range(len(pts) + 1):
+            if j == i:
                 continue
-            triangle_1 = getABC(a, b, pts[i])
-            triangle_2 = getABC(a, b, pts[j])
-
-            if semelhantes(triangle_1, triangle_2):
-                tree.append(triangle_1)
-                tree.append(triangle_2)
-                answer += 2
-                if triangle_2 in tree and i > 0:
-                    answer -= 1
-    return answer
+            if pointIsInPolygon(polygon, pts[i]):
+                tree[i].append(j)
 
 
 if __name__ == "__main__":
@@ -57,8 +31,6 @@ if __name__ == "__main__":
         x, y = map(int, input().split(" "))
         points.append(Point(x, y))
 
-    # Primeira abordagem: calculo a semelhança entre os triângulos
-    # Pela relação AB/DE = BC/EF = CA/FD
-    # Comparo par a par
-    # Caclular a semelhança é linear, comparar não
+    # Segunda abordagem: Determinar se o ponto está no polígono (a maioria são triângulos?) possivelmente não convexo.
+    # Retorna 1 para pontos estritamente externos. e 0 para pontos restantes.
     print(solve(A, B, points))
