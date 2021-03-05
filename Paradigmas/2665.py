f0018d95f94b6b986d1b2af66f09e90c2a8cd9e3
isoBello <isabellamfreitas2@gmail.com>
2021-03-05 11:57:39 -0300

    Accepted

--- Paradigmas/2665.py
+++ Paradigmas/2665.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict
 
-tree = defaultdict(list)
-INT_MAX = 10000
-
 
 class Point:
     def __init__(self, xCoord, yCoord):
@@ -11,16 +8,75 @@ class Point:
         self.y = yCoord
 
 
-def solve(a, b, pts):
+class Graph:
+    def __init__(self, N):
+        self.vertices = N
+        self.edges = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.edges[u].append(v)
+
+
+def solve(a, b, pts, G):
     for i in range(len(pts)):
-        polygon = [a, pts[i], b]
+        triangle = [a, b, pts[i]]
         for j in range(len(pts)):
-            if pts[j] in polygon:
+            if pts[j] in triangle:
                 continue
-            if PointIsInPolygon(polygon, pts[j]):
-                tree[i].append(j)
+            if pointIsInside(triangle, pts[j]):
+                G.addEdge(i, j)
 
 
+def pointIsInside(a, b):
+    # Area of triangle ABi
+    areaABi = area(a[0].x, a[0].y,
+                   a[1].x, a[1].y,
+                   a[2].x, a[2].y)
+    # Area of triangle jBC
+    areajBC = area(b.x, b.y,
+                   a[1].x, a[1].y,
+                   a[2].x, a[2].y
+                   )
+    # Area of triangle AjC
+    areaAjC = area(a[0].x, a[0].y,
+                   b.x, b.y,
+                   a[2].x, a[2].y)
+    # Area of triangle ABj
+    areaABj = area(a[0].x, a[0].y,
+                   a[1].x, a[1].y,
+                   b.x, b.y)
+
+    # If the sum of areajBC, areaAjC and areaABj is same as areaABi
+    if areaABi == (areajBC + areaAjC + areaABj):
+        return True
+    return False
+
+
+def area(a, b, c, d, e, f):
+    return abs((a * (d - f) + c * (f - b)
+                + e * (b - d)) / 2.0)
+
+
+def findLongestPath(G, N):
+    M = [0] * (N + 1)
+    visited = [False] * (N + 1)
+
+    for i in range(N):
+        if not visited[i]:
+            DFS(i, G, M, visited)
+    answer = 0
+    for i in range(N):
+        answer = max(answer, M[i])
+
+    return answer + 1
+
+
+def DFS(node, G, M, visited):
+    visited[node] = True
+    for u in G.edges[node]:
+        if not visited[u]:
+            DFS(u, G, M, visited)
+        M[node] = max(M[node], 1 + M[u])
 
 
 if __name__ == "__main__":
@@ -30,13 +86,11 @@ if __name__ == "__main__":
     # Ancoras
     A = Point(Xa, 0)
     B = Point(Xb, 0)
+    graph = Graph(P)
 
     for point in range(P):
         x, y = map(int, input().split(" "))
         points.append(Point(x, y))
 
-    # Segunda abordagem: Determinar se o ponto está no polígono (a maioria são triângulos?) possivelmente não convexo.
-    # Retorna True para pontos estritamente externos, e False para pontos restantes.
-    solve(A, B, points)
-
-
+    solve(A, B, points, graph)
+    print(findLongestPath(graph, P))
