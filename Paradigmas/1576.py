8ced8db0cf32d831b73dce6ee355b3475ff2997c
isoBello <isabellamfreitas2@gmail.com>
2021-03-11 22:19:11 -0300

    not working

--- Paradigmas/1576.py
+++ Paradigmas/1576.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
-def solve(builds):
-    if len(builds) == 1:
-        return [[builds[0][0], builds[0][1]],
-                [builds[0][2], 0]]
+def solve(B):
+    if not B:
+        return []
+    if len(B) == 1:
+        return [[B[0][0], B[0][1]], [B[0][2], 0]]
 
-    mid = len(builds) // 2
-    left = solve(builds[:mid])
-    right = solve(builds[mid:])
+    mid = len(B) // 2
+    left = solve(B[:mid])
+    right = solve(B[mid:])
     return merge(left, right)
 
 
 def merge(A, B):
     hA, hB = 0, 0
     i, j = 0, 0
-    fly_path = []
+    skyline = []
 
     while i < len(A) and j < len(B):
         if A[i][0] < B[j][0]:
@@ -23,40 +24,27 @@ def merge(A, B):
             hB, x = B[j][1], B[j][0]
             j += 1
         else:
-            hA, hb = A[i][1], B[j][1]
+            hA, hB = A[i][1], B[j][1]
             x = B[j][0]
             i += 1
             j += 1
-
-        top = max(hA, hB)
-        condition = check_top(fly_path, top)
-        if condition:
-            fly_path.append([x, top])
-
-    fly_path = check_remains(fly_path, A, B, i, j)
-    return fly_path
+        k = max(hA, hB)
+        if valid_top(skyline, k):
+            skyline.append([x, k])
+    skyline.extend(B[j:])
+    skyline.extend(A[i:])
+    return skyline
 
 
-def check_remains(llist, a, b, i, j):
-    if i < len(a):
-        llist.extend(a[i:])
-    if j < len(b):
-        llist.extend(b[j:])
-    return llist
-
-
-def check_top(list, at_top):
-    if list:
-        if list[-1][1] != at_top:
-            return True
-    return not list
+def valid_top(llist, k):
+    return not llist or llist[-1][1] != k
 
 
 def print_skyline(S):
     output = ''
-    for i in range(len(S)):
-        a, b = str(S[i][0]), str(S[i][1])
-        if i == 0:
+    for s in range(len(S)):
+        a, b = str(S[s][0]), str(S[s][1])
+        if s == 0:
             output += a + " " + b
         else:
             output += " " + a + " " + b
@@ -73,7 +61,7 @@ if __name__ == "__main__":
         except (EOFError, ValueError) as e:
             break
 
-    skyline = solve(buildings)
-    print_skyline(skyline)
+    answer = solve(buildings)
+    print_skyline(answer)
 
 
