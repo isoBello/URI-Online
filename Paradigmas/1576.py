43f75c0e77217296d2aa28e81c440189208605cf
isoBello <isabellamfreitas2@gmail.com>
2021-03-11 23:20:59 -0300

    not working

--- Paradigmas/1576.py
+++ Paradigmas/1576.py
@@ -1,43 +1,61 @@
 # -*- coding: utf-8 -*-
-def solve(B):
-    if not B:
-        return []
-    if len(B) == 1:
-        return [[B[0][0], B[0][1]], [B[0][2], 0]]
+class Building:
+    def __init__(self, x, y, z):
+        self.left = x
+        self.height = y
+        self.right = z
 
-    mid = len(B) // 2
-    left = solve(B[:mid])
-    right = solve(B[mid:])
-    return merge(left, right)
+
+def solve(building, left, right):
+    if left == right:
+        view = [[building[left].left, building[left].height], [building[left].right, 0]]
+        return view
+
+    mid = (left + right) // 2
+    f_build = solve(building, left, mid)
+    s_build = solve(building, mid + 1, right)
+
+    return merge(f_build, s_build)
 
 
 def merge(A, B):
-    hA, hB = 0, 0
-    i, j = 0, 0
     skyline = []
-
-    while i < len(A) and j < len(B):
+    i, j = 0, 0
+    h1, h2 = 0, 0
+    while (i < len(A)) and (j < len(B)):
         if A[i][0] < B[j][0]:
-            hA, x = A[i][1], A[i][0]
+            x, h1 = A[i][0], A[i][1]
+            skyline = append(skyline, [x, max(h1, h2)])
             i += 1
-        elif B[j][0] < A[i][0]:
-            hB, x = B[j][1], B[j][0]
+        elif A[i][0] == B[j][0]:
+            h1, h2 = A[i][1], B[j][1]
+            x = A[i][0]
+            skyline = append(skyline, [x, max(h1, h2)])
+            i += 1
             j += 1
         else:
-            hA, hB = A[i][1], B[j][1]
-            x = B[j][0]
-            i += 1
+            h2, x = B[j][1], B[j][0]
+            skyline = append(skyline, [x, max(h1, h2)])
             j += 1
-        k = max(hA, hB)
-        if valid_top(skyline, k):
-            skyline.append([x, k])
-    skyline.extend(B[j:])
-    skyline.extend(A[i:])
+    skyline = check_integrality(skyline, i, j, A, B)
     return skyline
 
 
-def valid_top(llist, k):
-    return not llist or llist[-1][1] != k
+def check_integrality(ans, i, j, l1, l2):
+    ans.extend(l1[i:])
+    ans.extend(l2[j:])
+    return ans
+
+
+def append(S, llist):
+    n = len(S)
+    if n > 0 and S[n - 1][0] == llist[0]:
+        S[n - 1][1] = max(S[n - 1][1], llist[1])
+        return S
+    elif n > 0 and S[n - 1][1] == llist[1]:
+        return S
+    S.append(llist)
+    return S
 
 
 def print_skyline(S):
@@ -57,11 +75,10 @@ if __name__ == "__main__":
     while True:
         try:
             L, H, R = map(int, input().split(" "))
-            buildings.append([L, H, R])
+            buildings.append(Building(L, H, R))
         except (EOFError, ValueError) as e:
             break
 
-    answer = solve(buildings)
+    answer = solve(buildings, 0, len(buildings) - 1)
     print_skyline(answer)
-
 
